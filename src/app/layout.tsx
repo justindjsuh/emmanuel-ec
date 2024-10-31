3c22db1793c88c3462f3f01a16991f7903f1fcf2
Justin Suh <jsuh@Justins-MBP.fios-router.home>
2024-10-31 12:09:52 -0400

    font and icon fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Montserrat } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const montserrat = Montserrat({ subsets: ["latin"], display: "swap" });
 
@@ -14,6 +15,28 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+            <Head>
+                <link
+                    rel="icon"
+                    type="image/png"
+                    href="/favicon-96x96.png"
+                    sizes="96x96"
+                />
+                <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+                <link rel="shortcut icon" href="/favicon.ico" />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/apple-touch-icon.png"
+                />
+                <link rel="manifest" href="/site.webmanifest" />
+                <link
+                    rel="preload"
+                    href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&display=swap"
+                    as="style"
+                    crossOrigin="anonymous"
+                />
+            </Head>
             <body className={montserrat.className}>{children}</body>
         </html>
     );
